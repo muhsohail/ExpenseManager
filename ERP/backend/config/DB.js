1a52d82765d3dd1eeebf34edc28f25ecedee57c2
Muhammad Sohail <mirza.sohail@hotmail.com>
2018-12-25 13:29:39 +0400

    updated package.json

--- ERP/backend/config/DB.js
+++ ERP/backend/config/DB.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //DB: 'mongodb://localhost:27017/ng6crud'
-    DB: 'mongodb+srv://sohail:King1982_@cluster0-yehxk.mongodb.net/test?retryWrites=true'
+    DB: 'mongodb://localhost:27017/ng6crud'
+    //DB: 'mongodb+srv://sohail:King1982_@cluster0-yehxk.mongodb.net/test?retryWrites=true'
  };

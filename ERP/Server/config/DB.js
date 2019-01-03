0433fdba0c5dcebdb83e5b6a4054aa1fc4267b42
Muhammad Sohail <mirza.sohail@hotmail.com>
2019-01-03 11:16:38 +0400

    added .env to gitGnore

--- ERP/Server/config/DB.js
+++ ERP/Server/config/DB.js
@@ -1,3 +1,6 @@
 module.exports = {
     DB: 'mongodb://localhost:27017/ng6crud'
+    //DB: 'mongodb+srv://admin:King_28041002@cluster0-yehxk.mongodb.net/ExpenseManager?retryWrites=true'
+   
  };
+ console.log(process.env);

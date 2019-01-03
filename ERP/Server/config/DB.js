6a246d3b7cd5d852c9cc22a5afa829440a63df69
Muhammad Sohail <mirza.sohail@hotmail.com>
2019-01-03 14:28:21 +0400

    Added env virables in connectionstring

--- ERP/Server/config/DB.js
+++ ERP/Server/config/DB.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
 module.exports = {
-    DB: 'mongodb://localhost:27017/ng6crud'
-    //DB: 'mongodb+srv://admin:King_28041002@cluster0-yehxk.mongodb.net/ExpenseManager?retryWrites=true'
-   
+    //DB: 'mongodb://localhost:27017/ng6crud'
+  
+    DB: 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+ '@cluster0-yehxk.mongodb.net/ExpenseManager?retryWrites=true'
  };
- console.log(process.env);
